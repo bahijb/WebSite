9ba2b5e59674c19f095da0657cfd007d34171de2
bahijb <bahijb@live.unc.edu>
2017-05-05 15:36:39 -0400

    Add files via upload

---
+++ example.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World</p>'; ?>  </body>
+</html>
+
+<!-- Localized -->
